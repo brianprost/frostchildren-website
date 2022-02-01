7d940fd2cb19e2f5ae3cbc82b6f3c95f5d6c5b3f
Brian Prost <brian.s.prost@gmail.com>
2022-01-31 16:27:23 -0800

    Once upon a time, there was no doc title. No more.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
 import { FrostLogo } from "../components/FrostLogo.component";
 
 export default function Home() {
   return (
-    <div className="bg-black touch-none overflow-clip">
-      <div className="flex justify-center w-screen h-screen items-center">
-        <FrostLogo />
+    <>
+      <Head>
+        <title>Frost Children</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="bg-black touch-none overflow-clip">
+        <div className="flex justify-center w-screen h-screen items-center">
+          <FrostLogo />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
