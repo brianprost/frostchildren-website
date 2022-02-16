db3147dea03df896b429fd2ec8130acb0a102e86
Brian Prost <brian.s.prost@gmail.com>
2022-02-16 14:16:04 -0800

    all that for some damn dynamic hover colors

---
+++ components/FooterLink.component.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const FooterLink = ({ link, displayTitle }) => {
+  const [hoverColor, setHoverColor] = useState("white");
+
+  const albumCoverColors = ["#b9d4ff", "#a25b6c", "#175B28"];
+
+  //   achived...no thanks to Tailwind
+  const toggleLinkColor = (isHover) => {
+    isHover
+      ? setHoverColor(albumCoverColors[Math.floor(Math.random() * (3 - 0) + 0)])
+      : setHoverColor("white");
+  };
+
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <div
+        className="py-4 text-5xl text-center hover:text-6xl"
+        onMouseEnter={() => toggleLinkColor(true)}
+        onMouseLeave={() => toggleLinkColor(false)}
+      >
+        {/* <SocialIcon icon={link.title} /> */}
+        <p className="font-bold" style={{ color: `${hoverColor}` }}>
+          {displayTitle}
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default FooterLink;
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,12 @@ import { FrostLogo } from "../components/FrostLogo.component";
 
 export default function Home() {
   return (
-    <div className="bg-black bg-opacity-80 overflow-hidden">
+    <div className="bg-black overflow-hidden">
       <Head>
         <title>Frost Children</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="relative w-full max-w-lg min-h-screen">
-        <div className="absolute top-24 -left-4 w-72 h-72  bg-sky-600 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
-        <div className="absolute top-24 -right-4 w-72 h-72 bg-yellow-200 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
-        <div className="absolute bottom-20 left-20 w-72 h-72  bg-green-500 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
         <div className="relative">
           <div className="grid grid-cols-1 grid-rows-2 w-screen h-screen items-center">
             <div className="row-span-1 w-full h-full lg:container lg:w-1/2 lg:mx-auto flex justify-center items-center">
