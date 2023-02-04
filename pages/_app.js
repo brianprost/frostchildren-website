15573978cff0ffbaa8b2167d67906ad96c5a347a
Brian Prost <brian.s.prost@gmail.com>
2023-02-03 19:26:26 -0500

    installed google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import "@fontsource/rowdies";
+import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize('G-L77HN1FM9H');
   return <Component {...pageProps} />;
 }
 
