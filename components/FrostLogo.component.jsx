5f01a754d8a99de4170d5123587e0169fcb12ac0
Brian Prost <brian.s.prost@gmail.com>
2023-02-04 13:34:57 -0500

    made it so that it doesn't play 100 different versions of the audio if you spam click it

--- components/FrostLogo.component.jsx
+++ components/FrostLogo.component.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import useSound from "use-sound";
 
 export const FrostLogo = () => {
-  const [play] = useSound("./audio/how_to_summon_an_abrys_v2.m4a");
+  const [play, { stop, isPlaying }] = useSound(
+    "./audio/how_to_summon_an_abrys_v2.m4a"
+  );
 
+  // make sure that it doesn't play twice
+  function playAudio() {
+    if (!isPlaying) {
+      play();
+    }
+  }
   const frostImages = `img/abrys.webp`;
 
   return (
     <div className="w-auto h-auto">
-      <img src={frostImages} alt="frost-children-logo" onClick={play} />
+      <img src={frostImages} alt="frost-children-logo" onClick={playAudio()} />
     </div>
   );
 };
