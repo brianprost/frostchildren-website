77f378663169cbf0bc2c1174e6f4d05bb791b232
Brian Prost <brian.s.prost@gmail.com>
2023-05-01 20:40:45 -0400

    api also now records to firestore

--- pages/api/promote-it-on-abrys-fam.js
+++ pages/api/promote-it-on-abrys-fam.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { IgApiClient } from "instagram-private-api";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
 
 async function postToInstagram(url, discordUser) {
   const ig = new IgApiClient();
@@ -14,8 +28,10 @@ async function postToInstagram(url, discordUser) {
       file: buffer,
       caption: "Promoted on @abrys_fam by Discord user " + discordUser,
     });
+    return true;
   } catch (e) {
     console.log("🤖" + e);
+    return false;
   }
 }
 
@@ -26,11 +42,30 @@ export default async function handler(req, res) {
   }
   // due to the edge runtime, and my laziness, we need to reject PNG photos for now
   if (req.body.url.endsWith(".png")) {
-    res.status(400).json({ error: "PNG photos are not supported. I know...annoying. Tell SleepRides to get on it." });
+    res.status(400).json({
+      error:
+        "PNG photos are not supported. I know...annoying. Tell SleepRides to get on it.",
+    });
     return;
   }
-  const { url, caption } = req.body;
-  const publishResult = await postToInstagram(url, discordUser);
+  const { url, discordUser } = req.body;
+  const didPromoteToAbrysFamInstagram = await postToInstagram(url, discordUser);
+  const imageFileName = url
+    .split("/")
+    .pop()
+    ?.split(".")[0]
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  await setDoc(
+    doc(
+      firestore,
+      `discord/bots/promote-it-on-abrys-fam/${discordUser}_${imageFileName}`
+    ),
+    {
+      promotedOnAbrysFam: didPromoteToAbrysFamInstagram,
+    },
+    { merge: true }
+  );
 
-  res.status(200).json({ popstarStatus: JSON.stringify(publishResult) });
+  res.status(200).json({ success: didPromoteToAbrysFamInstagram });
 }
