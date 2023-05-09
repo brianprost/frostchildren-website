aff03e3ff44434feef694f76dc5aff2939c622ed
Brian Prost <brian.s.prost@gmail.com>
2023-05-09 10:59:48 -0400

    Newsletter popup (#3)

---
+++ components/Newsletter.jsx
@@ -0,0 +1,61 @@
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { BiX } from "react-icons/bi";
+
+export default function Newsletter() {
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full justify-center p-4 text-center items-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-gray-400 px-2 py-2 sm:flex sm:flex-row-reverse h-[80vh] rounded-md">
+                  <BiX
+                    className="absolute text-4xl top-6 right-6 text-gray-50 cursor-pointer"
+                    onClick={() => setOpen(false)}
+                  />
+                  <iframe
+                    src="https://digital.umusic.com/frostchildren-signup"
+                    width="100%"
+                    height="100%"
+                    className="rounded-md"
+                  />
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
