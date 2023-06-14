4b89247bd58685c5e3dde451af45aa4c47f70af8
Brian Prost <brian.s.prost@gmail.com>
2023-06-14 13:51:39 -0400

    Add Discord interactions to API endpoint

--- pages/api/promote-it-on-abrys-fam.js
+++ pages/api/promote-it-on-abrys-fam.js
@@ -1,34 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { verifyKey } from 'discord-interactions'; // You may need to install this package
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      await verifyRequestSignatures(req, res, req.body);
-    //   await handlePostRequest(req, res);
-    return res.status(200).json({ type: 1 });
-    } catch (err) {
-      console.error(err);
-      res.status(401).end("Unauthorized");
-    }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+export default function handler(req, res) {
+  const signature = req.headers['x-signature-ed25519'];
+  const timestamp = req.headers['x-signature-timestamp'];
+  const json = JSON.stringify(req.body);
+  const isVerified = verifyKey(json, signature, timestamp, process.env.DISCORD_PUBLIC_KEY);
+
+  if (!isVerified) {
+    res.status(401).send('Invalid request signature');
+    return;
   }
-}
 
-function verifyRequestSignatures(req, res, buf) {
-  const signature = req.headers["x-hub-signature"];
-  if (!signature) {
-    throw new Error("Couldn't validate the signature.");
-  } else {
-    const elements = signature.split("=");
-    const signatureHash = elements[1];
-    const expectedHash = crypto
-      .createHmac("sha1", process.env.SECRET)
-      .update(buf)
-      .digest("hex");
-    if (signatureHash !== expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
+  switch (req.body.type) {
+    case 1: // Ping event
+      res.status(200).json({ type: 1 });
+      break;
+    default:
+      // Handle other interaction types here
+      res.status(200).json({
+        type: 4,
+        data: {
+          tts: false,
+          content: "Congrats on sending your command!",
+          embeds: [],
+          allowed_mentions: { parse: [] },
+        },
+      });
+      break;
   }
 }
