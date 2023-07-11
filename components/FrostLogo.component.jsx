f07e4890beeb8feb9745c1b9d9ad6b88fce27247
Brian Prost <me@brianprost.com>
2023-07-11 19:13:21 -0400

    NO MORE ALARMINGLY LOUD BELL

--- components/FrostLogo.component.jsx
+++ components/FrostLogo.component.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import useSound from "use-sound";
 
 export const FrostLogo = () => {
-  const [play, { stop, isPlaying }] = useSound(
-    "./audio/how_to_summon_an_abrys_v2.m4a"
-  );
-
-  // make sure that it doesn't play twice
-  function playAudio() {
-    if (!isPlaying) {
-      play();
-    }
-  }
   const frostImages = `img/abrys.webp`;
 
   return (
     <div className="w-auto h-auto">
-      <img src={frostImages} alt="frost-children-logo" onClick={() => playAudio()} />
+      <img src={frostImages} alt="frost-children-logo" />
     </div>
   );
 };
