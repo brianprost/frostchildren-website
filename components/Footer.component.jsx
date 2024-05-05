a40170405aafcbce8811d29ee11ed5e8341db157
Brian Prost <brian.s.prost@gmail.com>
2024-05-05 18:26:23 -0400

    Changed frost children merch link

--- components/Footer.component.jsx
+++ components/Footer.component.jsx
@@ -10,7 +10,7 @@ const links = [
     displayTitle: "TOUR",
   },
   {
-    link: "https://frostchildren.limitedrun.com",
+    link: "https://frostchildren.bandcamp.com/merch"
     key: "Merch",
     displayTitle: "MERCH",
   },
