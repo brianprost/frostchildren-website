d013b66bc7e28543afbef6b8289e794504e526f5
Brian Prost <me@brianprost.com>
2024-10-07 22:17:48 -0400

    new merch links (per region)
    not proud of this

--- components/Footer.component.jsx
+++ components/Footer.component.jsx
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FooterLink from "./FooterLink.component";
 import SocialIcon from "./SocialIcon.component";
 
-const links = [
-  // sup tailwind. i want to use dynamic classnames for hover colors. AND YOU'RE MAKING ME WISH I USED STYLED COMPONENTS
-  {
-    link: "/tour",
-    key: "Tour",
-    displayTitle: "TOUR",
-  },
-  {
-    link: "https://frostchildren.bandcamp.com/merch",
-    key: "Merch",
-    displayTitle: "MERCH",
-  },
-  {
-    link: "https://songwhip.com/frostchildren",
-    key: "Music",
-    displayTitle: "MUSIC",
-  },
-  {
-    link: "https://digital.umusic.com/frostchildren-signup",
-    key: "Newsletter",
-    displayTitle: "nEwsleTTer",
-  },
-  {
-    link: "https://discord.gg/qZn6BqJXej",
-    key: "Discord",
-    displayTitle: "DISCORD",
-  },
-  {
-    link: "https://www.instagram.com/thefrostchildren/",
-    key: "Instagram",
-    displayTitle: "INSTA",
-  },
-  {
-    link: "https://www.youtube.com/channel/UCIfMtvBbD-D34HEtC2ezeJw",
-    key: "YouTube",
-    displayTitle: "VIDS",
-  },
-];
-
 const Footer = () => {
+  const [merchLink, setMerchLink] = useState("");
+
+  useEffect(() => {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const isEuropeOrUK = /^(Europe|GB)\//.test(userTimeZone);
+    setMerchLink(
+      isEuropeOrUK
+        ? "https://stores.allotment.pro/frost-children/"
+        : "https://frost-children.myshopify.com/"
+    );
+  }, []);
+
+  const links = [
+    {
+      link: "/tour",
+      key: "Tour",
+      displayTitle: "TOUR",
+    },
+    {
+      link: merchLink,
+      key: "Merch",
+      displayTitle: "MERCH",
+    },
+    {
+      link: "https://songwhip.com/frostchildren",
+      key: "Music",
+      displayTitle: "MUSIC",
+    },
+    {
+      link: "https://digital.umusic.com/frostchildren-signup",
+      key: "Newsletter",
+      displayTitle: "nEwsleTTer",
+    },
+    {
+      link: "https://discord.gg/qZn6BqJXej",
+      key: "Discord",
+      displayTitle: "DISCORD",
+    },
+    {
+      link: "https://www.instagram.com/thefrostchildren/",
+      key: "Instagram",
+      displayTitle: "INSTA",
+    },
+    {
+      link: "https://www.youtube.com/channel/UCIfMtvBbD-D34HEtC2ezeJw",
+      key: "YouTube",
+      displayTitle: "VIDS",
+    },
+  ];
+
   return (
     <section id="footer-section" className="text-white">
       <div className="flex flex-col justify-center sm:grid sm:grid-cols-2 sm:mt-28 sm:mb-4 md:mt-4">
