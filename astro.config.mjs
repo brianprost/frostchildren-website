6cb1ebac0adb767bffa745b0303fcce22033b80b
Brian Prost <me@brianprost.com>
2025-06-02 20:17:17 -0400

    do cloudflare instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,35 @@
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import keystatic from "@keystatic/astro";
-import compress from "@playform/compress";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel";
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import keystatic from "@keystatic/astro"
+import compress from "@playform/compress"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.frostchildren.xyz",
+	site: "https://www.frostchildren.xyz",
 
-  redirects: {
-      "/admin": "/keystatic",
+	redirects: {
+		"/admin": "/keystatic"
 	},
 
-  integrations: [
-      react(),
-      keystatic(),
-      sitemap(),
-      compress({
-          HTML: true,
-          JavaScript: true,
-          CSS: false,
-          Image: false,
-          SVG: false,
-      }),
+	integrations: [
+		react(),
+		keystatic(),
+		sitemap(),
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: false,
+			Image: false,
+			SVG: false
+		})
 	],
 
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()]
 	},
 
-  adapter: vercel(),
-});
+	adapter: cloudflare()
+})
